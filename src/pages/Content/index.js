02ff30b7826f97cef3ecdae5615a2fe8362b5561
Pedro-dev-083 <pedroazevedo83@hotmail.com>
2023-04-30 18:48:03 -0300

    Tratamento de erro

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Movies } from "./Movies";
 import { Books } from "./Books";
+import { addMovie } from "../../services/api";
 
 export const Content = () => {
     const [content, setContent]  = useState('');
@@ -8,6 +9,20 @@ export const Content = () => {
     const showBooks = (content === '' || content === 'books')
     const handleContent = (value) =>{
         setContent(value);
+    }
+
+    const add = (e) =>{
+        e.preventDefault();
+        const name = document.getElementById("name").value;
+        const genre = document.getElementById("genre").value;
+        const year = document.getElementById("year").value;
+        addMovie(name, genre, year)  
+        .then(response => {
+            console.log(response.message); // faz algo com a resposta de sucesso
+          })
+          .catch(error => {
+            console.error(error.message); // faz algo com o erro retornado
+          });;        
     }
 
     return (
@@ -19,6 +34,17 @@ export const Content = () => {
             <button onClick={() => handleContent('movies')}>Filmes</button>
             {showMovies ? <Movies/> : null}
             {showBooks ? <Books/> : null}
+            <div>
+                <h2>Adicionar filme:</h2>
+                <label>Nome:</label>
+                <input id="name"/>
+                <label>Gênero:</label>
+                {/* TODO: Fazer com que o input apenas aceite as respostas de gênero existentes no banco */}
+                <input id="genre"/>
+                <label>Ano:</label>
+                <input id="year"/>
+                <button onClick={e=>add(e)}>Enviar</button>
+            </div>
         </>
     )
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,30 @@
-import axios from 'axios';
+import axios from "axios";
 
 //TODO: ajeitar o backend para enviar em requisição HTTPS
-const BASE = 'http://localhost:8080'
+const BASE = "http://localhost:8080";
 
-export const getMovies= async () => {
-    const response = await axios.get(`${BASE}/movies`);
-    return response.data;
+export const getMovies = async () => {
+  const response = await axios.get(`${BASE}/movies`);
+  return response.data;
+};
+
+export const addMovie = async (nome, genero, ano) => {
+  const content = {
+    nome: nome,
+    genero: genero,
+    ano: ano,
   };
+  try {
+    const response = await axios.post(`${BASE}/movies`, content, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return { success: true, message: response.data.message };
+  } catch (err) {
+    if (err.response && err.response.data && err.response.data.message) {
+      return { success: false, message: err.response.data.message };
+    }
+    return { success: false, message: "Erro ao adicionar filme." };
+  }
+};
